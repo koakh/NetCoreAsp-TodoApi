83b237b5c593d28221516c6c93061bf4a97c0745
Mário Monteiro <mail@koakh.com>
2016-09-23 23:29:38 +0100

    ..

--- src/NetCoreAsp-TodoApi/Models/TodoItem.cs
+++ src/NetCoreAsp-TodoApi/Models/TodoItem.cs
@@ -7,7 +7,6 @@ namespace NetCoreAspTodoApi.Models
 {
     public class TodoItem
     {
-        //
         public string Key { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
