8141513c1435309271fa76cdac2fefb2d5865480
Mário Monteiro <mail@koakh.com>
2016-09-23 23:30:55 +0100

    ...

--- src/NetCoreAsp-TodoApi/Models/TodoItem.cs
+++ src/NetCoreAsp-TodoApi/Models/TodoItem.cs
@@ -10,6 +10,5 @@ namespace NetCoreAspTodoApi.Models
         public string Key { get; set; }
         public string Name { get; set; }
         public bool IsComplete { get; set; }
-        //
     }
 }
